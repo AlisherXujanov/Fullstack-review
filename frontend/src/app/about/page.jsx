be9007c5b264dbd75af22837b0f0fd93f8062d77
Alisher <alisherxujanov163@gmail.com>
2025-04-16 16:24:05 +0500

    deletion

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import Heading from "../components/Heading";
+
+function About() {
+    return (
+        <>
+            <Heading>About</Heading>
+            <p>lorem ipsum dolor sit amet</p>
+        </>
+    )
+}
+
+export default About;
---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Heading from "./components/Heading";
+
+export default function Home() {
+  return (
+    <div className={'lending-page-wrapper'}>
+      <Heading>Home page</Heading>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quos sunt tempora fugit vero numquam minus voluptatum molestias. Debitis quo eligendi enim illo, nesciunt nobis porro esse laudantium et officia!</p>
+    </div>
+  );
+}
