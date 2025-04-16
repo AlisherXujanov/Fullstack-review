10ebe7e5872c3d137197360685f32ebab7101aff
Alisher <alisherxujanov163@gmail.com>
2025-04-16 16:40:04 +0500

    update

---
+++ frontend/src/app/components/Nav/index.jsx
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (
+        <nav>
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </nav>
+    );
+}
+
+export default NavBar;
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import NavBar from "./components/Nav";
 
 
 export default function RootLayout({ children }) {
