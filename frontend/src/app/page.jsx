d8225169ef892bf55bc25de162621c286ed7d027
Alisher <alisherxujanov163@gmail.com>
2025-04-21 17:28:14 +0500

    update

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,13 +2,21 @@ import Heading from "./components/Heading";
 import Paragraph from "./components/Paragraph";
 import Searchbox from "./components/Searchbox";
 
+const NAMES = ["Sardor", "John", "Jane", "Jim", "Jill",
+  "Jamshed", "Amal", "Shoxa", "Istam", "Komil"]
+const FRUITS = ["Apple", "Banana", "Cherry", "Date", "Elderberry",
+  "Fig", "Grape", "Honeydew", "Kiwi", "Lemon"]
+const BRANDS = ["Apple", "Samsung", "Google", "Microsoft", "Amazon", "Tesla",
+  "Nvidia", "AMD", "Intel", "Sony", "LG", "Panasonic", "Sony", "LG", "Panasonic",
+]
+
 export default function Home() {
   return (
     <div className={'lending-page-wrapper'}>
       <Heading>Home page</Heading>
       <Paragraph>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quos sunt tempora fugit vero numquam minus voluptatum molestias. Debitis quo eligendi enim illo, nesciunt nobis porro esse laudantium et officia!
-        
+
         <br />
         <br />
         <Searchbox />
